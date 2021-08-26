aa6b9b6f7cf58c5e205caec1918221ed0413d320
devleandrodias <leandrodbdias@gmail.com>
2021-08-26 11:31:12 -0300

    finish guessing game

--- 01_introduction_language/guessing_game.c
+++ 01_introduction_language/guessing_game.c
@@ -18,48 +18,84 @@ int main()
   int win = 0;
   int attempts = 0;
   double score = 1000;
+  int tryNumber = 0;
+  int difficult = 0;
 
-  while (1)
+  printf("(1) - Easy \n(2) - Medium \n(3) - Hard\n");
+  printf("Choice: ");
+  scanf("%d", &difficult);
+
+  switch (difficult)
   {
-    attempts++;
+  case 1:
+    tryNumber = 20;
+    break;
+  case 2:
+    tryNumber = 15;
+    break;
+  case 3:
+    tryNumber = 10;
+    break;
+  default:
+    printf("Invalid option!");
+    break;
+  }
 
-    printf("\nAttempt: %d\n", attempts);
+  if (tryNumber > 0)
+  {
 
-    printf("\nDigit your estimate: ");
-    scanf("%d", &userEstimate);
-
-    printf("\nYour estimate is %d \n", userEstimate);
-
-    if (userEstimate < 0)
+    for (int i = 1; i <= tryNumber; i++)
     {
-      printf("\nNegative numbers are not valid!\n");
-      continue;
+      attempts++;
+
+      printf("\nAttempt: %d\n", attempts);
+
+      printf("\nDigit your estimate: ");
+      scanf("%d", &userEstimate);
+
+      printf("\nYour estimate is %d \n", userEstimate);
+
+      if (userEstimate < 0)
+      {
+        printf("\nNegative numbers are not valid!\n");
+        continue;
+      }
+
+      int greater = userEstimate > secretNumber;
+
+      win = secretNumber == userEstimate;
+
+      if (win)
+      {
+        break;
+      }
+
+      else if (greater)
+      {
+        printf("\nYour estimate is greater than the secret number\n");
+      }
+
+      else
+      {
+        printf("\nYour estimate is less than the secret number\n");
+      }
+
+      double lostPoints = (userEstimate - secretNumber) / (double)2;
+
+      score -= fabs(lostPoints);
     }
 
-    int greater = userEstimate > secretNumber;
-
-    if (secretNumber == userEstimate)
+    if (win)
     {
       printf("\nYOU WIN!\n");
-      break;
-    }
-
-    else if (greater)
-    {
-      printf("\nYour estimate is greater than the secret number\n");
+      printf("\nYOU WON IN %dst ATTEMP! AND YOUR SCORE IS %.1fpt\n", attempts, score);
     }
 
     else
     {
-      printf("\nYour estimate is less than the secret number\n");
+      printf("\nGAME OVER! TRY AGAIN!\n");
     }
-
-    double lostPoints = (userEstimate - secretNumber) / (double)2;
-
-    score -= fabs(lostPoints);
   }
-
-  printf("\nGAME OVER! YOU WON IN %dst ATTEMP! AND YOUR SCORE IS %.1fpt\n", attempts, score);
 
   return 0;
 }
