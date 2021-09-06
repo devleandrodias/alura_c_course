db2a438b656e46147b7d52ee5aba57cc86c04e0e
devleandrodias <leandrodbdias@gmail.com>
2021-09-06 20:44:17 -0300

    learning about header files, I/O, end finish game

--- 02_advancing_in_language/hangman_game.c
+++ 02_advancing_in_language/hangman_game.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
+#include "hangman_game.h"
+
+// Global variables
 char secretWord[20];
 char estimates[26];
 int estimate = 0;
+
+void addNewWord()
+{
+  char option;
+  printf("Do you want to add a new word in the game? (y/n) ");
+  scanf(" %c", &option);
+
+  if (option == 'y')
+  {
+    char newWord[20];
+    printf("What new word? ");
+    scanf("%s", newWord);
+
+    FILE *file;
+
+    file = fopen("words.txt", "r+");
+
+    if (file == 0)
+    {
+      printf("\nSorry! Database unavailable!\n");
+      exit(1);
+    }
+
+    int quantityWords;
+    fscanf(file, "%d", &quantityWords);
+    quantityWords++;
+
+    fseek(file, 0, SEEK_SET);
+    fprintf(file, "%d", quantityWords);
+
+    fseek(file, 0, SEEK_END);
+    fprintf(file, "\n%s", newWord);
+
+    fclose(file);
+  }
+}
 
 void showHeader()
 {
@@ -14,7 +55,26 @@ void showHeader()
 
 void chooseWord()
 {
-  sprintf(secretWord, "TESLA");
+  FILE *file = fopen("words.txt", "r");
+
+  if (file == 0)
+  {
+    printf("\nSorry! Database unavailable!\n");
+    exit(1);
+  }
+
+  int quantityWords;
+  fscanf(file, "%d", &quantityWords);
+
+  srand(time(0));
+  int random = rand() % quantityWords;
+
+  for (int i = 0; i < random; i++)
+  {
+    fscanf(file, "%s", secretWord);
+  }
+
+  fclose(file);
 }
 
 void kick(int *estimate, char estimates[26])
@@ -65,11 +125,52 @@ void drawGallows()
   printf("\n");
 }
 
+int lost()
+{
+  int errors = 0;
+
+  for (int i = 0; i < estimate; i++)
+  {
+    int exists = 0;
+
+    for (int j = 0; j < strlen(secretWord); j++)
+    {
+      if (estimates[i] == secretWord[j])
+      {
+        exists = 1;
+        break;
+      }
+    }
+
+    if (!exists)
+    {
+      errors++;
+    }
+  }
+
+  if (errors >= 5)
+    printf("\nGAME OVER! YOU LOST!\n");
+
+  return errors >= 5;
+}
+
+int win()
+{
+  for (int i = 0; i < strlen(secretWord); i++)
+  {
+    if (!alreadyKicked(secretWord[i]))
+    {
+      return 0;
+    }
+  }
+
+  printf("\nGAME OVER! YOU WIN!\n");
+
+  return 1;
+}
+
 int main()
 {
-  int win = 0;
-  int lost = 0;
-
   showHeader();
 
   chooseWord();
@@ -80,7 +181,9 @@ int main()
 
     kick(&estimate, estimates);
 
-  } while (!win && !lost);
+  } while (!win() && !lost());
+
+  addNewWord();
 
   return 0;
 }
---
+++ 02_advancing_in_language/hangman_game.h
@@ -0,0 +1,11 @@
+// Header files
+
+void kick(int *estimate, char estimates[26]);
+void showHeader();
+void addNewWord();
+void chooseWord();
+void drawGallows();
+
+int alreadyKicked(char letter);
+int win();
+int lost();
